44dbcd140b9c4fb90682b02cc6eb5cf449682552
Andrew Gray <andrew.aj.gray@gmail.com>
2020-11-23 21:09:28 +1030

    refactored to further reduced nested if statements

--- GildedRose.cs
+++ GildedRose.cs
@@ -15,82 +15,74 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (IsLegendary(i)) continue;
-
-                if (ShouldIncreaseQualityOfItem(i))
-                {
-                    IncreaseQualityOfItem(i);
-
-                    if (IsABackstagePass(i))
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            IncreaseQualityOfItem(i);
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            IncreaseQualityOfItem(i);
-                        }
-                    }
-                }
-                else
-                {
-                    ReduceQualityOfItem(i);
-                }
+                if (IsLegendary(i)) 
+                    continue;
 
                 DecreaseSellInOfItem(i);
 
-                if (Items[i].SellIn >= 0)
-                {
-                    continue;
-                }
-
-                if (Items[i].Name == "Aged Brie")
-                {
-                    IncreaseQualityOfItem(i);
-                    continue;
-                }
-
                 if (IsABackstagePass(i))
                 {
-                    Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                    if (Items[i].SellIn < 0)
+                    {
+                        Items[i].Quality = 0;
+                        continue;
+                    }
+
+                    IncreaseQualityOfItem(i);
+
+                    if (Items[i].SellIn < 10)
+                        IncreaseQualityOfItem(i);
+
+                    if (Items[i].SellIn < 5)
+                        IncreaseQualityOfItem(i);
+
                     continue;
                 }
+
+                if (IsAgedBrie(i))
+                {
+                    IncreaseQualityOfItem(i);
+
+                    if (Items[i].SellIn >= 0)
+                        continue;
+
+                    IncreaseQualityOfItem(i);
+
+                    continue;
+                }
+                
+                ReduceQualityOfItem(i);
+
+                if (Items[i].SellIn >= 0)
+                    continue;
 
                 ReduceQualityOfItem(i);
             }
         }
 
-        private bool IsConjured(int i) => Items[i].Name == "Conjured Mana Cake";
+        private bool IsConjured(int i) => Items[i].Name.StartsWith("Conjured");
 
-        private bool IsABackstagePass(int i) => Items[i].Name == "Backstage passes to a TAFKAL80ETC concert";
+        private bool IsABackstagePass(int i) => Items[i].Name.StartsWith("Backstage passes");
 
-        private bool ShouldIncreaseQualityOfItem(int i) => Items[i].Name == "Aged Brie" || Items[i].Name == "Backstage passes to a TAFKAL80ETC concert";
+        private bool IsAgedBrie(int i) => Items[i].Name == "Aged Brie";
 
         private bool IsLegendary(int i) => Items[i].Name == "Sulfuras, Hand of Ragnaros";
 
         private void IncreaseQualityOfItem(int i)
         {
-            if (Items[i].Quality < 50)
-            {
-                Items[i].Quality = Items[i].Quality + 1;
-            }
+            Items[i].Quality = Items[i].Quality + 1;
+
+            if (Items[i].Quality > 50)
+                Items[i].Quality = 50;
         }
 
         private void DecreaseSellInOfItem(int i)
         {
-            if (!IsLegendary(i))
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
+            Items[i].SellIn = Items[i].SellIn - 1;
         }
 
         private void ReduceQualityOfItem(int i)
         {
-            if (IsLegendary(i))
-                return;
-
             Items[i].Quality = Items[i].Quality - 1;
             
             if (IsConjured(i))
