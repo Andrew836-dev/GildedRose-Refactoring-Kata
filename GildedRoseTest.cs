0491d53b674355b07a791277121dc87d4f621e44
Andrew Gray <andrew.aj.gray@gmail.com>
2020-11-23 16:33:05 +1030

    added tests for initial requirements

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -7,12 +7,123 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void Item_Name_And_Order_Stay_The_Same()
+        {
+            IList<Item> Items = new List<Item> { 
+                new Item { Name = "foo", SellIn = 0, Quality = 0 },
+                new Item { Name = "bar", SellIn = 0, Quality = 0 }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("bar", Items[1].Name);
+        }
+
+        [Test]
+        public void Standard_Items_Decrease_Quality_Once()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(9, Items[0].Quality);
+        }
+
+        [Test]
+        public void When_SellIn_0_Standard_Items_Decrease_Quality_Twice()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(8, Items[0].Quality);
+        }
+
+        [Test]
+        public void Items_Decrease_SellIn_During_UpdateQuality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(9, Items[0].SellIn);
+        }
+
+        [Test]
+        public void Sulfuras_Is_Unchanged()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("Sulfuras, Hand of Ragnaros, 0, 80", Items[0].ToString());
+        }
+
+        [Test]
+        public void When_SellIn_0_Aged_Brie_Increases_Quality_Twice()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(Items[0].Quality, 12);
+        }
+
+        [Test]
+        public void Aged_Brie_Increases_Quality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(Items[0].Quality, 11);
+        }
+
+        [Test]
+        public void Backstage_Passes_Increases_Quality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(Items[0].Quality, 11);
+        }
+
+        [Test]
+        public void When_SellIn_10_Backstage_Passes_Increases_Quality_Twice()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(Items[0].Quality, 12);
+        }
+
+        [Test]
+        public void When_SellIn_5_Backstage_Passes_Increases_Quality_Thrice()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(Items[0].Quality, 13);
+        }
+
+        [Test]
+        public void When_SellIn_0_Backstage_Passes_Has_Quality_0()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(Items[0].Quality, 0);
+        }
+
+        [Test]
+        public void Quality_Stops_Decreasing_At_0()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual("foo, -1, 0", Items[0].ToString());
+        }
+
+        [Test]
+        public void Quality_Stops_Increasing_At_50()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 50 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("Aged Brie, -1, 50", Items[0].ToString());
         }
     }
 }
